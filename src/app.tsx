7490de833c47df4474095da46d6153a6076ebf8f
vinicius <vinicius@nextfit.com.br>
2025-05-29 13:39:37 -0300

    Publicando primeira versão com vite funcionando corretamente;

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+  /**
+   * App component.
+   * @returns {JSX.Element} The rendered component.
+   */
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Electron + React + Vite!</h1>
+      <p>Welcome to your new Electron application with React and Vite!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/renderer.tsx
@@ -0,0 +1,44 @@
+/**
+ * This file will automatically be loaded by vite and run in the "renderer" context.
+ * To learn more about the differences between the "main" and the "renderer" context in
+ * Electron, visit:
+ *
+ * https://electronjs.org/docs/tutorial/process-model
+ *
+ * By default, Node.js integration in this file is disabled. When enabling Node.js integration
+ * in a renderer process, please be aware of potential security implications. You can read
+ * more about security risks here:
+ *
+ * https://electronjs.org/docs/tutorial/security
+ *
+ * To enable Node.js integration in this file, open up `main.ts` and enable the `nodeIntegration`
+ * flag:
+ *
+ * ```
+ *  // Create the browser window.
+ *  mainWindow = new BrowserWindow({
+ *    width: 800,
+ *    height: 600,
+ *    webPreferences: {
+ *      nodeIntegration: true
+ *    }
+ *  });
+ * ```
+ */
+
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+console.log('👋 This message is being logged by "renderer.ts", included via Vite');z
